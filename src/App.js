533ebc53fec8b4b35dc249e8149bc9e332dca9ad
Roopamrc3 <82797057+Roopamrc3@users.noreply.github.com>
2022-02-17 10:58:42 +0530

    Updated app.js
    added some text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello from Rapid Innovation
+          Hello from Rapid Innovation Team
         </p>
         <a
           className="App-link"
